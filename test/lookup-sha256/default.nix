fa10cc42829f335a3f127e475f9b055f54f4b1ef
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-03-03 00:03:07 +1300

    Run tests against latest hackage to find issues

--- test/lookup-sha256/default.nix
+++ test/lookup-sha256/default.nix
@@ -4,7 +4,6 @@
     inherit compiler-nix-name;
     name         = "pandoc";
     version      = "2.9.2.1";
-    index-state  = "2020-12-17T00:00:00Z";
     # Function that returns a sha256 string by looking up the location
     # and tag in a nested attrset
     lookupSha256 = { location, tag, ... }:
